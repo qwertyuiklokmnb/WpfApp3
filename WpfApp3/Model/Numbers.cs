539680b8e507372d788bb4ffbbe195f4054996a6
qwertyuiklokmnb <168690863+qwertyuiklokmnb@users.noreply.github.com>
2024-05-02 10:09:35 +1000

    Add files via upload

---
+++ WpfApp3/Model/Numbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp3.ViewModel;
+
+namespace WpfApp3.Model
+{
+
+    class AreaCalculator
+    {
+        public double CalculateSquareArea(double sideLength)
+        {
+            return sideLength * sideLength;
+        }
+
+        public double CalculateEquilateralTriangleArea(double sideLength)
+        {
+            // Площадь равностороннего треугольника: (сторона^2 * корень из 3) / 4
+            return (sideLength * sideLength * Math.Sqrt(3)) / 4;
+        }
+    }
+}
---
+++ WpfApp3/ViewModel/MyViewModel.cs
@@ -0,0 +1,78 @@
+﻿
+using System.ComponentModel; 
+using System.Windows.Input; 
+using WpfApp3.Model; 
+
+namespace WpfApp3.ViewModel
+{
+    class MyViewModel : INotifyPropertyChanged // объявление класса MyViewModel, который реализует интерфейс INotifyPropertyChanged для уведомления об изменениях свойств
+    {
+        private bool isSquareSelected; 
+        private bool isTriangleSelected; 
+        private double area; 
+        private readonly AreaCalculator areaCalculator; 
+
+        public bool IsSquareSelected 
+        {
+            get { return isSquareSelected; } 
+            set 
+            {
+                isSquareSelected = value; 
+                if (isSquareSelected)
+                {
+                    CalculateSquareArea(); 
+                }
+                RaisePropertyChanged("IsSquareSelected"); // вызов метода RaisePropertyChanged для уведомления об изменении свойства
+            }
+        }
+
+        public bool IsTriangleSelected 
+        {
+            get { return isTriangleSelected; } 
+            set 
+            {
+                isTriangleSelected = value; 
+                if (isTriangleSelected)
+                {
+                    CalculateTriangleArea(); 
+                }
+                RaisePropertyChanged("IsTriangleSelected"); // вызов метода RaisePropertyChanged для уведомления об изменении свойства
+            }
+        }
+
+        public double Area
+        {
+            get { return area; } 
+            set 
+            {
+                area = value; 
+                RaisePropertyChanged("Area"); 
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); 
+        }
+
+        public MyViewModel() 
+        {
+            IsSquareSelected = false; 
+            IsTriangleSelected = false; 
+            Area = 0; 
+            areaCalculator = new AreaCalculator(); 
+        }
+
+        private void CalculateSquareArea() 
+        {
+            Area = areaCalculator.CalculateSquareArea(3); 
+        }
+
+        private void CalculateTriangleArea()
+        {
+            Area = areaCalculator.CalculateEquilateralTriangleArea(3); 
+        }
+    }
+}
